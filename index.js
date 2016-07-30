f92107022012bcf4587b8055c04a3d67587e8ef4
Simon McManus <mcmanus.simon@gmail.com>
2016-07-30 21:35:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+exports.readFile = function(file, options, callback) {
+
+    //var url = window.location.origin + '/base/spec/sample' +  file;
+    var url = file;
+
+    // no double slashes - hack
+    url = url.replace(/\/\//g, '\/');
+
+    if(window.fetch) {
+        fetch(url, {
+            method: 'get'
+        }).then(function(response) {
+            return response.text();
+        }).then(function(response) {
+            callback(null, response);
+        }).catch(function(err) {
+            console.log('err', err)
+        });
+    } else {
+        callback(new Error('Fetch not supported'));
+    }
+};
