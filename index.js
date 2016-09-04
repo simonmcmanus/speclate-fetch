de85da2ca5119e049fe00067bcc631aa51fddfdb
Simon McManus <mcmanus.simon@gmail.com>
2016-09-04 09:57:25 +0100

    if response.ok  not true throw an error and catch it later

--- index.js
+++ index.js
@@ -3,22 +3,19 @@
 require('whatwg-fetch');
 
 exports.readFile = function(file, options, callback) {
-    //var url = window.location.origin + '/base/spec/sample' +  file;
     var url = file;
-
-    // no double slashes - hack
-    url = url.replace(/\/\//g, '\/');
-
     fetch(url, {
         method: 'get'
     }).then(function(response) {
+
         if (!response.ok) {
-            return callback(Error(response.statusText));
+            throw Error(response.statusText);
         }
         return response.text();
     }).then(function(text) {
         callback(null, text);
     }).catch(function(err) {
-        console.log(err)
+        console.log(err, file)
+        callback(err)
     });
 };
