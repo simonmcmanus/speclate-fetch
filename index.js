5b6730589fd73331af2b6e9d0dcbfe7505e19bef
Simon McManus <mcmanus.simon@gmail.com>
2017-02-25 09:14:57 +0000

    remove what-wg deps

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 'use strict';
 
-require('whatwg-fetch');
 
+function loadXMLDoc(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+               callback(null, xmlhttp.responseText)
+           }
+           else {
+               callback(new Error('Not Found'))
+           }
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+var noFetch = function(url, callback) {
+    console.log('no fetch', url)
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+
+        console.log(xmlHttp.readyState, xmlHttp.status)
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        } else {
+            callback(new Error('Not Found'))
+        }
+
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
 
 var doFetch = function(url, type, callback) {
+
+    if(typeof fetch === 'undefined') {
+        return loadXMLDoc(url, callback);
+    }
+
     fetch(url, {
         method: 'get'
     }).then(function(response) {
